bd00fff064f2cc3f10fd040d3177b3fac89a74d9
민규 <120396128+Mingyu7@users.noreply.github.com>
2025-09-18 14:32:55 +0900

    set hosts 값 수정

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0#!cu7=0)so64i6@7z!i*ulqq!yfz!$u07ajt@7gvzjz!^!j7p
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
