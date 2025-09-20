4897a4338db3d9283f10fdb1343e22b60448daba
mingyu <vkans7526@naver.com>
2025-09-20 15:54:06 +0900

    mysql ec2연ì결 ì설정파잉ㄹ일 수정

--- config/settings.py
+++ config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-1',
+        'USER': 'admin',
+        'PASSWORD': 'mingyu5749',
+        'HOST': 'database-1.c894c6ocwin.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
