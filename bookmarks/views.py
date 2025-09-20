d9ef47dc08c85c972504d5ba75fcff665341ac55
mingyu <vkans7526@naver.com>
2025-09-20 14:26:40 +0900

    giwon ver2

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
-from .models import Favorite, Property
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from bookmarks.models import Favorite
+from listings.models import Property # Property 모델은 listings 앱에서 가져옵니다.
+from .models import Favorite # Favorite 모델은 현재 앱에서 가져옵니다.
+from django.http import HttpResponse, JsonResponse # JsonResponse 추가
 
 
 @login_required
 def favorites(request):
-    favorites = Favorite.objects.all()
-    user_favorites = Favorite.objects.filter(user_id=3)  # user_id에 맞게 변경
+    user_favorites = Favorite.objects.filter(user=request.user).select_related('property')
 
     context = {
         "favorites": user_favorites,
@@ -24,3 +22,23 @@ def remove_favorite(request, property_id):
     favorite = get_object_or_404(Favorite, user=request.user, property=property_to_remove)
     favorite.delete()
     return redirect('bookmarks:favorites')
+
+@login_required
+def toggle_bookmark(request, property_id):
+    if request.method == 'POST':
+        property_obj = get_object_or_404(Property, pk=property_id)
+        
+        # 현재 유저와 해당 매물에 대한 북마크(Favorite)를 찾습니다.
+        favorite, created = Favorite.objects.get_or_create(user=request.user, property=property_obj)
+
+        if created:
+            # 북마크가 새로 생성되었으면
+            bookmarked = True
+        else:
+            # 이미 존재하면 삭제합니다.
+            favorite.delete()
+            bookmarked = False
+
+        return JsonResponse({'bookmarked': bookmarked})
+    
+    return JsonResponse({'error': 'Invalid request'}, status=400)
