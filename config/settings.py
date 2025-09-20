1d173c7248d3195ef6023c9f5e5c27a84c710973
mingyu <vkans7526@naver.com>
2025-09-20 16:19:19 +0900

    init 파일 mysql 연동2

--- config/settings.py
+++ config/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'pymysql.install_as_MySQLdb',
         'NAME': 'database-1',
         'USER': 'admin',
         'PASSWORD': 'mingyu5749',
