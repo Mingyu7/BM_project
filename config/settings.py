75ddc33363dcbbf791d57babe834d99fbca73fe2
tmdtlr <tmdtlr4766@naver.com>
2025-09-22 17:29:46 +0900

    매물목록 ìcss변경 다시 푸시

--- config/settings.py
+++ config/settings.py
@@ -83,6 +83,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# sqllite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#mysql
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
